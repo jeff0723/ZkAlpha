056310bdd629b43e577259ffbfd76233358f9e4b
jeffreylin0723 <qwertzxc1478963@gmail.com>
2023-07-22 21:16:16 +0200

    fix build

--- packages/frontend/src/pages/trader/create/index.tsx
+++ packages/frontend/src/pages/trader/create/index.tsx
@@ -5,56 +5,56 @@ import { CheckIcon, ChevronUpDownIcon } from '@heroicons/react/20/solid'
 type Props = {}
 
 const frequecies = [
-'M',
-"D",
-"W"
+    'M',
+    "D",
+    "W"
 ]
 const assetPairs = [
     'ETH/ USDX'
 ]
-const index = (props: Props) => {
+const CreateVaultPage = (props: Props) => {
     const [assetPair, setAssetPair] = useState()
 
-  return (
-    <div className='flex justify-center'>
-        <div className='flex flex-col gap-4'>
-        <div className='text-center'>Create a vault</div>
-        <div className='flex flex-col gap-3 rounded-md bg-[#121219] p-4 min-w-[600px]'>
-            <div>
-                Name 
+    return (
+        <div className='flex justify-center'>
+            <div className='flex flex-col gap-4'>
+                <div className='text-center'>Create a vault</div>
+                <div className='flex flex-col gap-3 rounded-md bg-[#121219] p-4 min-w-[600px]'>
+                    <div>
+                        Name
+                    </div>
+
+                    <input className='px-2 py-3 outline-0 bg-[#1b1b21] rounded-md'></input>
+                    <div>
+                        Frequency
+                    </div>
+                    <select id="frequency" name="frequency" className="block w-full px-2 py-3 outline-0 bg-[#1b1b21] rounded-md">
+                        <option value="Daily">Daily</option>
+                        <option value="Weekly">Weekly</option>
+                        <option value="Monthly">Monthly</option>
+                    </select>
+
+                    <div>
+                        Asset Pair
+                    </div>
+                    <select id="asset-pair" name="asset-pair" className="block w-full px-2 py-3 outline-0 bg-[#1b1b21] rounded-md">
+                        <option value="WETH/USDC">WETH/USDC</option>
+                        <option value="WBTC/USDC">WBTC/USDC</option>
+                    </select>
+                    <div>
+                        Model Hash
+                    </div>
+                    <input className='px-2 py-3 outline-0 bg-[#1b1b21] rounded-md'></input>
+                    <div className='pt-8 w-full'>
+                        <button className='w-full p-4 rounded-md font-bold bg-white text-black hover:border-white hover:border-2 hover:bg-black hover:text-white'>
+                            Create
+                        </button>
+                    </div>
+                </div>
             </div>
 
-            <input className='px-2 py-3 outline-0 bg-[#1b1b21] rounded-md'></input>
-            <div>
-                Frequency
-            </div>
-  <select id="frequency" name="frequency" className="block w-full px-2 py-3 outline-0 bg-[#1b1b21] rounded-md">
-    <option value="Daily">Daily</option>
-    <option value="Weekly">Weekly</option>
-    <option value="Monthly">Monthly</option>
-  </select>
-
-            <div>
-                Asset Pair
-            </div>
-            <select id="asset-pair" name="asset-pair" className="block w-full px-2 py-3 outline-0 bg-[#1b1b21] rounded-md">
-    <option value="WETH/USDC">WETH/USDC</option>
-    <option value="WBTC/USDC">WBTC/USDC</option>
-  </select>
-            <div>
-                Model Hash
-            </div>
-            <input className='px-2 py-3 outline-0 bg-[#1b1b21] rounded-md'></input>
-            <div className='pt-8 w-full'>
-            <button className='w-full p-4 rounded-md font-bold bg-white text-black hover:border-white hover:border-2 hover:bg-black hover:text-white'> 
-        Create
-        </button>
         </div>
-        </div>
-        </div>
-      
-    </div>
-  )
+    )
 }
 
-export default index
+export default CreateVaultPage
--- packages/frontend/src/pages/trader/index.tsx
+++ packages/frontend/src/pages/trader/index.tsx
@@ -20,32 +20,32 @@ const Card = styled.div`
     border-radius: 8px;
     padding:16px;
 `
-const index = (props: Props) => {
+const TraderHomePage = (props: Props) => {
     const router = useRouter()
-  return (
-    <div className='flex justify-center items-center gap-10'>
-        <StrategyCard address='0x1234' name='MA-200' assetPair='ETH/USDC' frequency='Monthly' active={true} performance={0.23} tvl={500000} isTrader={true} />
-        <Card>
-            <div className='flex flex-col gap-[12px]'>
-            <PlusIcon
-                    className="h-14 w-14 text-violet-200 hover:text-violet-100"
-                    aria-hidden="true"
-            />
-            <div className='text-[16px] font-bold'>Create Alpha Vault</div>
-            <div className='text-sm text-gray-400'> Upload your alpha model and create a vault to earn commission</div>
-            </div>
-            <button 
-            onClick={()=>{
-                router.push('/trader/create')
-            }}
-            className='mt-14 w-full p-4 rounded-md font-bold bg-white text-black hover:border-white hover:border-2 hover:bg-black hover:text-white'> 
-        Create
-        </button>
-        
+    return (
+        <div className='flex justify-center items-center gap-10'>
+            <StrategyCard address='0x1234' name='MA-200' assetPair='ETH/USDC' frequency='Monthly' active={true} performance={0.23} tvl={500000} isTrader={true} />
+            <Card>
+                <div className='flex flex-col gap-[12px]'>
+                    <PlusIcon
+                        className="h-14 w-14 text-violet-200 hover:text-violet-100"
+                        aria-hidden="true"
+                    />
+                    <div className='text-[16px] font-bold'>Create Alpha Vault</div>
+                    <div className='text-sm text-gray-400'> Upload your alpha model and create a vault to earn commission</div>
+                </div>
+                <button
+                    onClick={() => {
+                        router.push('/trader/create')
+                    }}
+                    className='mt-14 w-full p-4 rounded-md font-bold bg-white text-black hover:border-white hover:border-2 hover:bg-black hover:text-white'>
+                    Create
+                </button>
 
-        </Card>
-    </div>
-  )
+
+            </Card>
+        </div>
+    )
 }
 
-export default index
+export default TraderHomePage
