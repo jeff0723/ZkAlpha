3eda919ae35b677fb0ec33c7e73c9620561e9fc0
justaliang <justaliang@protonmail.com>
2023-07-22 18:40:48 +0200

    merge

--- packages/contracts/contracts/Relayer.sol
+++ packages/contracts/contracts/Relayer.sol
@@ -3,12 +3,31 @@ pragma solidity ^0.8.17;
 
 import {MerkleTreeWithHistory, IHasher} from "./MerkleTreeWithHistory.sol";
 import {ERC20} from "solmate/tokens/ERC20.sol";
-import {Verifier} from "./Verifier.sol";
 import {Vault} from "./Vault.sol";
+import {
+    IDepositVerifier,
+    IWithdrawVerifier,
+    ISwapVerifier,
+    IFinalizeVerifier
+} from "./IVerifier.sol";
+
+struct ModelOutput {
+    uint8 direction;
+    uint248 amount;
+}
+
+struct ModelInput {
+    uint256 chainlinkPrice;
+}
 
 contract Relayer is MerkleTreeWithHistory {
-    ERC20 public immutable TOKEN;
-    Verifier public verifier;
+    
+    ERC20 immutable public TOKEN;
+    IDepositVerifier public depositVerifier;
+    IWithdrawVerifier public withdrawVerifier;
+    ISwapVerifier public swapVerifier;
+    IFinalizeVerifier public finalizeVerifier;
+    ModelInput public modelInput;
     mapping(bytes => Vault) public vaultMap;
 
     mapping(bytes32 => bool) public nullifierHashes;
@@ -16,12 +35,45 @@ contract Relayer is MerkleTreeWithHistory {
 
     constructor(
         ERC20 _token,
-        Verifier _verifier,
+        IDepositVerifier _depositVerifier,
+        IWithdrawVerifier _withdrawVerifier,
+        ISwapVerifier _swapVerifier,
+        IFinalizeVerifier _finalizeVerifier,
         IHasher _hasher,
         uint32 _merkleTreeHeight
     ) MerkleTreeWithHistory(_merkleTreeHeight, _hasher) {
         TOKEN = _token;
-        verifier = _verifier;
+        depositVerifier = _depositVerifier;
+        withdrawVerifier = _withdrawVerifier;
+        swapVerifier = _swapVerifier;
+        finalizeVerifier = _finalizeVerifier;
+    }
+
+    function deposit() public {
+
+    }
+
+    function withdraw() public {
+
+    }
+
+    function finalize() public {
+
+    }
+
+    function transact(
+        bytes calldata _proof,
+        bytes32 _nullifier,
+        ModelOutput calldata modelOutput
+    ) public {
+        // uint256[] _publicInputs = new uint256[](4);
+        // _publicInputs[0] = roots[0];
+        // _publicInputs[1] = uint256(_nullifier);
+
+        // require(
+        //     swapVerifier.verify(_publicInputs, _proof),
+        //     "Swap Verifier failed"
+        // );
     }
 
     /*//////////////////////////////////////////////////////////////
