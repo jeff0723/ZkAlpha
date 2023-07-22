11e7cfcbc1a7b1ebb12376416a0139d6e416a127
justaliang <justaliang@protonmail.com>
2023-07-22 10:10:26 +0200

    setup foundry

---
+++ packages/contracts/test/foundry/Counter.t.sol
@@ -0,0 +1,14 @@
+// SPDX-License-Identifier: UNLICENSED
+pragma solidity ^0.8.13;
+
+import "forge-std/Test.sol";
+import "../../contracts/Counter.sol";
+
+contract CounterTest is Test {
+    Counter public counter;
+
+    function setUp() public {
+        counter = new Counter();
+        counter.setNumber(0);
+    }
+}
