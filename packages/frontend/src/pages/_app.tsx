d1d7bc759743d463e09e2bd04d17ee74f163d34c
jeffreylin0723 <qwertzxc1478963@gmail.com>
2023-07-22 10:42:15 +0200

    add split path logic

--- packages/frontend/src/pages/_app.tsx
+++ packages/frontend/src/pages/_app.tsx
@@ -1,6 +1,10 @@
 import '@/styles/globals.css'
 import type { AppProps } from 'next/app'
 
-export default function App({ Component, pageProps }: AppProps) {
+export default function App({ Component, pageProps, router }: AppProps) {
+  const isLandingPage = router.pathname === '/';
+  if (isLandingPage) {
+    return <Component {...pageProps} />
+  }
   return <Component {...pageProps} />
 }
