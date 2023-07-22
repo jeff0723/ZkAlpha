85c8bc8f9b4ae17dda7af5c83393d0338b35c4ae
jeffreylin0723 <qwertzxc1478963@gmail.com>
2023-07-23 00:17:35 +0200

    add direct to home page

--- packages/frontend/src/components/Header.tsx
+++ packages/frontend/src/components/Header.tsx
@@ -1,12 +1,16 @@
 import React from 'react'
 import { ConnectButton } from '@rainbow-me/rainbowkit';
+import { useRouter } from 'next/router'
 
 type Props = {}
 
 const Header = (props: Props) => {
+    const router = useRouter()
     return (
         <div className='p-4 flex w-full justify-between'>
-            <div className='font-bold'>ZKAlpha</div>
+            <div className='font-bold text-[24px]' onClick={() => {
+                router.push('/')
+            }}>ZKAlpha</div>
             <ConnectButton />
         </div>
     )
