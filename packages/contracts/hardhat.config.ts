a2b555f3824333315ddb204ef4ad6a7e0ee1b6fc
justaliang <justaliang@protonmail.com>
2023-07-22 10:51:14 +0200

    setup hardhat.config.ts

--- packages/contracts/hardhat.config.ts
+++ packages/contracts/hardhat.config.ts
@@ -1,8 +1,45 @@
 import { HardhatUserConfig } from "hardhat/config";
 import "@nomicfoundation/hardhat-toolbox";
+import "hardhat-deploy";
+import "hardhat-contract-sizer";
 
 const config: HardhatUserConfig = {
-  solidity: "0.8.19",
+  solidity: "0.8.17",
+  contractSizer: {
+    alphaSort: true,
+    disambiguatePaths: false,
+    runOnCompile: false,
+    strict: true,
+    only: [],
+  },
+  gasReporter: {
+    enabled: process.env.REPORT_GAS !== undefined,
+    currency: "USD",
+  },
+  namedAccounts: {
+    deployer: 0,
+  },
+  networks: {
+    hardhat: {
+      mining: {
+        auto: !(process.env.HARDHAT_DISABLE_AUTO_MINING === "true"),
+        interval: [100, 3000],
+      },
+      accounts: {
+        mnemonic: "test test test test test test test test test test test junk",
+      },
+      chainId: 1337
+    },
+    gnosis: {
+      accounts: process.env.PRIVATE_KEY ? [process.env.PRIVATE_KEY] : [],
+      url: "https://rpc.gnosischain.com",
+    },
+  },
+  etherscan: {
+    apiKey: {
+      gnosis: process.env.GNOSIS_API_KEY ?? "",
+    }
+  },
 };
 
 export default config;
