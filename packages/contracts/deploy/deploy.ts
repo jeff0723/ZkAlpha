9df5737beb3e0586633d80fdf25234f59b5082e8
taetaehoho <75167060+taetaehoho@users.noreply.github.com>
2023-07-22 23:40:42 +0200

    deploy scripts

--- packages/contracts/deploy/deploy.ts
+++ packages/contracts/deploy/deploy.ts
@@ -1,12 +1,26 @@
 import { HardhatRuntimeEnvironment } from 'hardhat/types';
 import { DeployFunction } from 'hardhat-deploy/types';
-import { ethers, network, run } from "hardhat";
+import { network, run } from "hardhat";
+import { ethers } from "ethers"
+import {erc20abi} from "../abis/erc20-abi.js"
 
 const func: DeployFunction = async function (hre: HardhatRuntimeEnvironment) {
 	const { deployments, getNamedAccounts } = hre;
 	const { deploy } = deployments;
 
 	const { deployer } = await getNamedAccounts();
+
+	console.log(network.name)
+
+	// get the RPC URL for that network
+	const provider = ethers.getDefaultProvider()
+
+	const usdcContractAddress = "usdc_contract_address";
+	const usdcContract = new ethers.Contract(usdcContractAddress, erc20abi, provider);
+  
+	const wethContractAddress = "weth_contract_address";
+	const wethContract = new ethers.Contract(wethContractAddress, erc20abi, provider);
+
 
     /**
      *  ERC20 _asset,
@@ -21,9 +35,8 @@ const func: DeployFunction = async function (hre: HardhatRuntimeEnvironment) {
         address _oneInchAddress;
      */
 
-	
 
-    const vaultargs = [, "vault", "VAL", 
+    const vaultargs = [wethContract, "vault", "VAL", 
     "0xfa04032c159aE60d53d34d3811406Fb53E5A3215", "0xfa04032c159aE60d53d34d3811406Fb53E5A3215", 
     3600, 3600, 3600, "0xfa04032c159aE60d53d34d3811406Fb53E5A3215", "0x1111111254EEB25477B68fb85Ed929f73A960582"]
 
